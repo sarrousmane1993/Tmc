d8761439d9206dd021c226bf0fb9babefe29983d
Ousmane SARR <sarrbayeousmane@gmail.com>
2018-09-16 08:14:26 +0000

    Premier commit

---
+++ src/sn/objis/tmc/presentation/Tmc.java
@@ -0,0 +1,14 @@
+package sn.objis.tmc.presentation;
+
+public class Tmc {
+
+	public static void main(String[] args) {
+		//Menus menus = new Menus();
+		//menus.menuGeneral();
+				String nom = "sarrr" ;
+				//char [] l=nom.toCharArray();
+				//System.out.println(l[1]);
+				
+	}
+
+}
